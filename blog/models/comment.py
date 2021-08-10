c3c8278c84727be90c86dd2fa39cb8a3b151fb6b
Resul Takak <resultakak@gmail.com>
2021-08-10 22:29:15 +0300

    Create comment model

---
+++ blog/models/comment.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from blog.models import Post
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comment')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_At = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'comment'
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
+
+    def __str__(self):
+        return self.comment
