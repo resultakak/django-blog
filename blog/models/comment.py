37868c254cb5b19724e27735ab6e649b93cfef57
Resul Takak <resultakak@gmail.com>
2021-08-10 23:49:35 +0300

    Fixed style

--- blog/models/comment.py
+++ blog/models/comment.py
@@ -3,8 +3,10 @@ from blog.models import Post
 
 
 class Comment(models.Model):
-    author = models.ForeignKey('account.CustomUserModel', on_delete=models.CASCADE, related_name='comment')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(
+        'account.CustomUserModel', on_delete=models.CASCADE, related_name='comment')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_At = models.DateTimeField(auto_now=True)
