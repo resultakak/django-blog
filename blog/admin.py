11fee319a587ec6f4ed24b4f64ca90127c058b0e
Resul Takak <resultakak@gmail.com>
2021-08-10 12:15:07 +0300

    update admin.py for post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from blog.models import Category, Post
 
-admin.site.register(Category);
-admin.site.register(Post);
+admin.site.register(Category)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'created_at', 'update_at'
+    )
+
+
+admin.site.register(Post, PostAdmin)
