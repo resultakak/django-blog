b09c6dd2ff5a59e4e9bcd6b47a2b2ade6ef2747c
Resul Takak <resultakak@gmail.com>
2021-08-10 23:20:25 +0300

    Fixed

--- blog/models/post.py
+++ blog/models/post.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     slug = AutoSlugField(populate_from='title', unique=True)
     content = RichTextField()
     categories = models.ManyToManyField(Category)
-    cover = models.ImageField(upload_to='uploads')
+    cover = models.ImageField(upload_to='uploads/post')
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey('account.CustomUserModel', on_delete=models.CASCADE, related_name='posts')
