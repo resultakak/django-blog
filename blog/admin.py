3e45a884dc0b6be00e220e5dd34afeb352d8847b
Resul Takak <resultakak@gmail.com>
2021-08-11 10:33:11 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,7 @@ from blog.models import Category, Post, Comment, Contact
 admin.site.register(Category)
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
     list_display = (
@@ -11,22 +12,13 @@ class PostAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Post, PostAdmin)
-
-
+@admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('author', 'created_at', 'updated_At')
     search_fields = ('author__username',)
 
 
-admin.site.register(Comment, CommentAdmin)
-
-
-
+@admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('fullname', 'created_at')
     search_fields = ('email',)
-
-
-admin.site.register(Contact, ContactAdmin)
-
