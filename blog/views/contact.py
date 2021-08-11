1a72e87bc1b4b456650ce627c36cc472affd995d
Resul Takak <resultakak@gmail.com>
2021-08-11 16:35:13 +0300

    Update contact.py

--- blog/views/contact.py
+++ blog/views/contact.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def contact(request):
-    return HttpResponse('<h1>Contact</h1>')
+    context = {
+        "title": "Contact"
+    }
+    return render(request, 'pages/contact.html', context=context)
